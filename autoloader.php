e2f710bb5cd249c872ed083457b67fcc24042e5e
Killsoft <killsoft1692@yandex.ua>
2014-10-27 19:54:21 +0200

    1.3.01

---
+++ autoloader.php
@@ -0,0 +1,12 @@
+<?php
+
+spl_autoload_register(function ($namespace)
+{
+    $filePath = str_replace("\\", "/",$namespace) . ".php";
+
+    if (file_exists($filePath)) {
+        require_once($filePath);
+    }else{
+        return;
+    }
+});
